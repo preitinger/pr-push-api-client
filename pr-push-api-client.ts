7223abb10e5680761cce55dabf29523b8978fef0
preitinger <peter.reitinger@gmail.com>
2024-02-07 09:56:15 +0100

    added some logs for testing

--- pr-push-api-client.ts
+++ pr-push-api-client.ts
@@ -1,5 +1,8 @@
 
 /**
+ * @deprecated 
+ * will be done differently with 'next-pwa'
+ * 
  * ask for permission for push messages.
  * If granted, register the service worker at serviceWorkerPath.
  * Then, registers a push subscription at the browser's push service.
@@ -10,27 +13,34 @@ export async function pushApiStart(
     onMessage: (e: unknown) => void
 ): Promise<PushSubscription> {
 
+    console.log('Notification.permission', Notification.permission);
     if (Notification.permission === 'denied') throw new Error('Notifications not permitted!');
     if (Notification.permission === 'default') {
         switch (await Notification.requestPermission()) {
             case 'granted':
+                console.log('permission now granted');
                 break;
             default:
                 throw new Error('Notifications not permitted!');
         }
     }
 
+    console.log('before getRegistrations');
     const oldRegs = await navigator.serviceWorker.getRegistrations();
     for (const oldReg of oldRegs) {
+        console.log('before unregister');
         await oldReg.unregister();
     }
 
-
+    console.log('before register');
     const registration = await navigator.serviceWorker.register(serviceWorkerPath);
     navigator.serviceWorker.onmessage = e => {
         onMessage(e);
     }
+    console.log('before wait for ready');
+    await navigator.serviceWorker.ready;
     try {
+        console.log('before subscribe');
         return await registration.pushManager.subscribe({
             userVisibleOnly: true,
             applicationServerKey: applicationServerKey
